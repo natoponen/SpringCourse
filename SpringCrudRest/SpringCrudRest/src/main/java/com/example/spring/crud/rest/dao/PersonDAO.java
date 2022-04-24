c50e482de97123e87360114eeb8619a9b3cd3096
Nikita Toponen <ntoponen@gmail.com>
2022-04-24 19:53:32 +0300

    Add lesson 23

--- SpringCrudRest/SpringCrudRest/src/main/java/com/example/spring/crud/rest/dao/PersonDAO.java
+++ SpringCrudRest/SpringCrudRest/src/main/java/com/example/spring/crud/rest/dao/PersonDAO.java
@@ -32,4 +32,14 @@ public class PersonDAO {
         person.setId(++PEOPLE_COUNT);
         people.add(person);
     }
+
+    public void update(int id, Person person) {
+        Person personToUpdate = show(id);
+
+        personToUpdate.setName(person.getName());
+    }
+
+    public void delete(int id) {
+        people.removeIf(person -> person.getId()==id);
+    }
 }
